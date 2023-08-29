9876db644486d737f8218923dc37f16f1de8a64d
liuxinyu <liuxinyu@superhii.com>
2023-08-29 10:39:47 +0800

    Vue文件上传

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Wedding from './App.vue'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(Wedding)
+})
---
+++ src/mock/data.js
@@ -0,0 +1,75 @@
+export default {
+  code: 
+`
+// 美好的事情
+// 愿与你分享
+// 我们要结婚啦
+😗新娘：陈丽;
+陕西咸阳人;
+😋新郎：刘欣宇;
+陕西韩城人;
+// 我们将结为夫妻;
+// 在你的见证与祝福下
+// 以后的日子里
+// 我们将一起前行！`,
+  executions: [{
+    name:'打包一切美好祝福',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'-平安-',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'-幸福-',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'-快乐-',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'-美满🙂-',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'-多财😄-',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'幸福开启中...',
+    time: '',
+    visible: false
+  }],
+  barrages: [
+    '66666666',
+    '愿你美梦成真',
+    '你的邀请函真是美',
+    '人美心更美',
+    '早生贵子',
+    '哇哇哇',
+    '还差女朋友吗',
+    '白头偕老',
+    '祝幸福~',
+    '恭喜恭喜啊',
+    '你真棒',
+    '羡慕羡慕 ',
+    '牛批哇',
+    '恭喜你',
+    '希望幸福美满',
+    '恭喜恭喜',
+    '哇',
+    '哈哈哈恭喜你',
+    '恭喜恭喜',
+    '恭喜你恭喜你',
+    '哇成家啦',
+    '祝幸福',
+    '郎才女貌~'
+  ],
+}
